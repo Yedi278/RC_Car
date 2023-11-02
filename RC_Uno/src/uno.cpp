04a5a8dbf444bfa2002b714e035fb60ecdada622
Yedi278 <yehan278@gmail.com>
2023-11-02 11:32:03 +0100

    controlli motore finiti

--- RC_Uno/src/uno.cpp
+++ RC_Uno/src/uno.cpp
@@ -7,44 +7,49 @@
 char c;
 AF_DCMotor motore(3);
 String receivedBuffer = "NULL";
+char charBuff[5];
+
+void motorControl();
+
 
 void setup() {
 
   motore.setSpeed(MAXSPEED);
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  // Serial.begin(9600);  // start serial for output
 
 }
 
 void loop() {
   
-  Wire.requestFrom(8, 4);    // request 6 bytes from peripheral device #8
+  Wire.requestFrom(8, 4);    // request 4 bytes from peripheral device #8
 
   while (Wire.available()) { // peripheral may send less than requested
     
-    c = Wire.read(); // receive a byte as character
+    c = Wire.read();         // receive a byte as character
     
     receivedBuffer.concat(c);
 
   }
-  Serial.println(receivedBuffer);
-  receivedBuffer = "";
-  delay(500);
 
+  motorControl();
+  receivedBuffer = "";
+  delay(5);
 }
 
 void motorControl(){
 
-  switch(receivedBuffer){
+  if      (receivedBuffer == "0011"){
 
-    default:
-      motore.run(RELEASE);
-      break;
+    motore.run(FORWARD);
 
-    case "0011":
-    
+  }else if(receivedBuffer == "0012"){
 
+    motore.run(BACKWARD);
+
+  }else if(receivedBuffer == "0013"){
+
+    motore.run(RELEASE);
 
   }
-
 }
